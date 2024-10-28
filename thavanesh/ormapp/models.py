688d5ae6ba3676fdee06dbd18b4d1db4193f6ddd
thavanesh <thavaneshb2006@gamil.com>
2024-10-28 15:02:03 +0530

    Success

---
+++ thavanesh/ormapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib import admin 
+class BankLoan(models.Model):
+  acc=models.IntegerField(primary_key="accno")
+  ph=models.IntegerField()
+  ifsc=models.CharField(max_length=100)
+  loanno=models.IntegerField()
+  adress=models.CharField(max_length=100)
+  pan=models.IntegerField()
+  adhar=models.IntegerField()
+
+class BankLoanAdmin(admin.ModelAdmin):
+ list_display=('acc','ph','ifsc','loanno','adress','pan','adhar',)
